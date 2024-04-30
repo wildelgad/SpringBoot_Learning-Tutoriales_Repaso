ae5adf96a909b936079e54ccf98e5e4460261ae0
Wilinton Delgado Bustamante <wilinton.delgado.bustamante.zuluaga>
2024-04-29 23:52:15 -0500

    Primer commit -> mirando capitulo 2 de Logs

---
+++ MorsaProgramando/holaSpring/src/main/java/com/morsaprogramando/holaSpring/HolaSpringApplication.java
@@ -0,0 +1,16 @@
+package com.morsaprogramando.holaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringApplication.class, args);
+
+		System.out.println("\n\tHola Spring");
+	}
+
+
+}
