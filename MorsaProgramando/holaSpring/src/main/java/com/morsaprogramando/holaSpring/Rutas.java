45969f8b8c7e47ceae986ef1da492deaa6b1b745
Wilinton Delgado Bustamante <wilinton.delgado.bustamante.zuluaga>
2024-04-30 01:56:24 -0500

    Commit 3 -> completado capitulo de PathParams

--- MorsaProgramando/holaSpring/src/main/java/com/morsaprogramando/holaSpring/Rutas.java
+++ MorsaProgramando/holaSpring/src/main/java/com/morsaprogramando/holaSpring/Rutas.java
@@ -2,6 +2,7 @@ package com.morsaprogramando.holaSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class Rutas {
         return "Hola mundo desde Spring Controller :)";
     }
 
-    //------------------ PathParam: ---------------
+    //------------------ PathParams: ---------------
     //-->Filtrando por ID del libro
     @GetMapping("/libro/{id}")
     String leerLibro(@PathVariable int id){
@@ -23,5 +24,13 @@ public class Rutas {
     @GetMapping("/libro/{id}/editorial/{editorial}")
     String leerLibroEditorial(@PathVariable int id, @PathVariable String editorial){
         return "Leyendo el libro id: " + id + ", editorial: " + editorial;
+    }
+
+
+    //------------------ QueryParams: ---------------
+    //-->
+    @GetMapping("/libro2/{id}")
+    String leerLibro2(@PathVariable int id, @RequestParam String params, @RequestParam String editorial){
+        return "Leyendo el libro2 id: " + id + ", params: " + params + ", La editorial es: " + editorial;
     }
 }
