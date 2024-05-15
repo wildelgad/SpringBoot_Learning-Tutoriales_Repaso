89c4a04bb6ddf38599590af6f0011ffab3bdb3df
Wilinton Delgado Bustamante <wilinton.delgado.bustamante.zuluaga>
2024-05-15 00:51:36 -0500

    Commit 10 --> Completado Video de HttpStatus I

--- MorsaProgramando/holaSpring/src/main/java/com/morsaprogramando/holaSpring/Rutas.java
+++ MorsaProgramando/holaSpring/src/main/java/com/morsaprogramando/holaSpring/Rutas.java
@@ -64,22 +64,45 @@ public class Rutas {
     }
 
     //============================================= Http Status: =============================================
-    @GetMapping("/saludar01")
+    @GetMapping("/saludoHttpStatus01")
     @ResponseStatus(value = HttpStatus.ACCEPTED)
     String miSegundaRutaConStatus01(){
         return "Aprendiendo statuses http en spring Boot";
     }
 
-    @GetMapping("/saludar02")
+    @GetMapping("/saludoHttpStatus02")
     @ResponseStatus(value = HttpStatus.MOVED_PERMANENTLY, reason = "Fue movida a la versión 2 de la API")
     String miSegundaRutaConStatus02(){
         return "Aprendiendo statuses http en spring Boot";
     }
 
+    @GetMapping("/saludoHttpStatus03/{garaje}")
+    public ResponseEntity<String> miSegundaRutaConStatus03(@PathVariable String garaje){
+        if(garaje.equals("individual")){
+            return new ResponseEntity<>("Tu carro esta bien protegido", HttpStatus.ACCEPTED);
+        }else {
+            return new ResponseEntity<>("Tu carro corre riesgos", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //============================================= Http Exceptions: =============================================
     @GetMapping("/animales/{lugar}")
     public ResponseEntity<String> getAnimales(@PathVariable String lugar){
         if(lugar.equals("granja")){
-            return "Caballo, vaca, oveja, gallina";
+            //return new ResponseEntity<>("Es un animal de granja", HttpStatus.CREATED);
+            return ResponseEntity.status(HttpStatus.OK).body("Es un animal de granja: caballo, vaca, oveja, gallina");
+        }else if(lugar.equals("selva")){
+            return ResponseEntity.status(HttpStatus.OK).body("mono, gorila, puma");
+        }else {
+            //return new ResponseEntity<>("no....", HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("no....");
         }
     }
+
+    //Ahora como manejar errores no controlados, ejs: un null pointer exceptions; aritmethics exceptions
+    @GetMapping("/calcular/{numero}")
+    public int getCalculo(@PathVariable int numero){
+        throw new NullPointerException("La clave del usuario es password123 y no deería leerse en Postman");
+    }
+
 }
